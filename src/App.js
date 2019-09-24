316be9f0373d37859f46e83bcc99255cd4b65b36
Student <example@example.com>
2019-09-24 15:47:59 -0700

    It uses hook (useState) first!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Count from './components/Count';
 
 function App() {
   return (
     <div className="App">
+      <Count />
     </div>
   );
 }
