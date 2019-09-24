7fd90834d126ff7dca3df73bb6eee351ff632bdb
Student <example@example.com>
2019-09-24 16:44:47 -0700

    It adds an effectHook example.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Count from './components/Count';
+import EffectHook from './components/EffectHook';
 
 function App() {
   return (
     <div className="App">
       <Count />
+      <EffectHook />
     </div>
   );
 }
--- src/components/Count.js
+++ src/components/Count.js
@@ -5,10 +5,27 @@ function Count() {
 
   return(
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count+1)}>Click me</button>
+    <p>You clicked {count} times</p>
+    <button onClick={() => setCount(count+1)}>Click me</button>
     </div>
   );
 }
+
+// This is exactly same things with class
+// class Count extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {count:0};
+//   }
+//
+//   render() {
+//     return(
+//       <div>
+//         <p>You clicked {this.state.count} times</p>
+//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>Click me</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default Count;
